551b7c758fae1c35fb10eb9ab07919f32bc29bbf
mmauro2152 <A01282152@itesm.mx>
2023-11-08 15:43:37 -0600

    recibos activos recolector

---
+++ CaritasReliefAPI/Extensions/RecolectorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CaritasReliefAPI.DBContext;
+using CaritasReliefAPI.Schema;
+
+namespace CaritasReliefAPI.Extensions
+{
+    [ExtendObjectType(typeof(Recolectores))]
+    public class RecolectorExtensions
+    {
+        public IQueryable<Recibos> GetRecibosActivos(
+            [Parent] Recolectores recolector,
+            SQLContext sqlContext,
+            string date,
+            int idRecolector)
+        {
+            return sqlContext.Recibos
+                .Where(r =>
+                    r.idDonante == recolector.idRecolector &&
+                    r.idRecolector == idRecolector &&
+                    r.fecha.Date.ToString() == date);
+        }
+    }
+}
